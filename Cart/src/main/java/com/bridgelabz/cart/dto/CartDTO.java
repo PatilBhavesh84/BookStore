160f093e7a1778a7b970278090eef1c1b47f1246
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-10-03 18:28:45 +0530

    Added:Book Store project

---
+++ Cart/src/main/java/com/bridgelabz/cart/dto/CartDTO.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cart.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class CartDTO {
+    private Integer userID;
+    private Integer bookID;
+    @NotNull(message = "Book Quantity Should Not Be Null ")
+    private Integer quantity;
+}
---
+++ Cart/src/main/java/com/bridgelabz/cart/exception/BookStoreException.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.cart.exception;
+
+public class BookStoreException extends RuntimeException {
+
+    public BookStoreException(String message) {
+        super(message);
+    }
+}
